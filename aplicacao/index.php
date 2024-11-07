7572a1b12e0395feffdc8c79196d133dafc2d9d8
Your Name <you@example.com>
2024-11-07 11:55:09 -0300

    feat: organização mvc

---
+++ aplicacao/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    # Comentário de linha única
+
+    // Comentário de linha única
+    
+    /*
+        Bloco de Comentário
+    */
+
+    echo "<h1> Bem vindo ao site </h1>"; //comando de escrita (saída)
+
+    print("<h2> Novo Texto </h2>"); // escreve texto 
+?>
